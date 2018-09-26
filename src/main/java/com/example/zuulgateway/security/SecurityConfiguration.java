2d9de82aa34da70a1d42dccfdf73dae115c01fb5
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-26 23:54:32 +0800

    Refactor: use multi configuration for spring security

--- src/main/java/com/example/zuulgateway/security/SecurityConfiguration.java
+++ src/main/java/com/example/zuulgateway/security/SecurityConfiguration.java
@@ -1,14 +1,10 @@
 package com.example.zuulgateway.security;
 
 import com.example.zuulgateway.security.filter.CustomBasicTokenFilter;
-import com.example.zuulgateway.security.filter.CustomSimpleTokenFilter;
-import com.example.zuulgateway.security.provider.CustomBasicAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,22 +13,41 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 import javax.sql.DataSource;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .anyRequest().authenticated()
-            .and()
-                .addFilterBefore(new CustomBasicTokenFilter(authenticationManager()), BasicAuthenticationFilter.class)
-                .addFilterBefore(new CustomSimpleTokenFilter(), CustomBasicTokenFilter.class)
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/api/**")
+                    .authorizeRequests()
+                        .anyRequest().authenticated()
+                    .and()
+                        .addFilterBefore(new CustomBasicTokenFilter(authenticationManager()), BasicAuthenticationFilter.class)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
     }
+
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/tokens")
+                    .authorizeRequests()
+                        .anyRequest().authenticated()
+                    .and()
+                        .addFilterBefore(new CustomBasicTokenFilter(authenticationManager()), BasicAuthenticationFilter.class)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
