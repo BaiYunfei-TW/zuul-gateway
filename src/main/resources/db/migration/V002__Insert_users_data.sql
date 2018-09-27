23986a167484f52c703b12393e5102ccecf243cf
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-27 15:03:30 +0800

    add new spy user

--- src/main/resources/db/migration/V002__Insert_users_data.sql
+++ src/main/resources/db/migration/V002__Insert_users_data.sql
@@ -1,7 +1,9 @@
 INSERT INTO users(id, username, password, enable) VALUES
 ('yunfei-id', 'yunfei', '123456', 1),
-('admin-id', 'admin', '123456', 1);
+('admin-id', 'admin', '123456', 1),
+('spy-id', 'spy', '123456', 1);
 
 INSERT INTO authorities(userId, authority) VALUES
 ('yunfei-id', 'ROLE_USER'),
-('admin-id', 'ROLE_ADMIN');
+('admin-id', 'ROLE_ADMIN'),
+('spy-id', 'ROLE_SPY');
